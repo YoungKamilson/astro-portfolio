ff49c85fbf272b240f33f2b07f899e4341477cb2
Kamil Kubiczek <95166936+YoungKamilson@users.noreply.github.com>
2024-12-02 09:14:21 +0100

    Update info.ts

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,8 +1,8 @@
 export const info = {
   baseUrl: "https://kamil-kubiczek.vercel.app",
   name: "Kamil Kubiczek",
-  jobDescription: "Frontend Vue/Nuxt Web Developer",
-  about: `I am 18 years old web developer from Poland. My passion is creating websites using various technologies, currently I'm learing Rust as well as backend development with it. I have several major projects to my credit, such as a new school website for my technical school. I have been continuously developing in the industry for 3 years. I gained knowledge in creating websites using various tools such as React, Vue.js, Nuxt 3, Firebase, Strapi, Supabase etc. I always try to choose the right technology for the demand. I encourage you to check out my projects below. You can reach me via email or my socials.
+  jobDescription: "Fullstack Web Developer",
+  about: `I am a creative web developer who is passionate about modern technologies. I try to stay up to date with the latest trends in the industry. I willingly and very often share my knowledge with others, because the development of the team is as important to me as my own. I am open to people and new technologies, and I especially appreciate DX while working. I love managing a team and tasks, as well as UI/UX design at a basic level, I believe I have a good sense of aesthetics. I enjoy solving problems, and I enjoy matching the right technologies to team, client and project requirements is natural to me. Because of this, I strive to be versatile and flexible, I enjoy learning new technologies and combining them with each other. Therefore, in my portfolio you will find a variety of frameworks and libraries, which I I use depending on the needs of the project.
   `,
   keywords: 'kamil kubiczek, kamil kubiczek web developer, kamil kubiczek olesnica strony, kamil kubiczek strony internetowe, website developer portfolio, web developer portfolio, frontend developer portfolio, terminal',
 
@@ -13,9 +13,8 @@ export const info = {
       startDate: "Sep 2023",
       endDate: "Present",
       description: [
-        "- Working on innovative crypto AI Nuxt 3 web app.",
-        "- Designing UI/UX of product.",
-        "- Managing web infrastructure and CI/CD.",
+         "My responsibilities The project involved the creation of a site to promote the Termin AI project, with a maintaining a retro style, with blog elements. During the work I was the manager of my team, managing it and tasks in the Agile methodology at ClickUp, I was also the main designer of the UI/UX and the main developer from the use of Strapi headless CMS in combination with GraphQL. I took care of the proper SEO of the site, and was also in charge of DevOps. Together with the team, I was also responsible for the design of the next version of the site AI and REST API term in Nuxt 3.",
+        "What I learned: During my work, I learned production use of Nuxt 3 and the REST API based on Nitro.js, built into Nuxt. I also mastered Gitlab and ClickUp in Agile methodology. As a manager, I had to communicate with the client. I encountered many problems during my DevOps work, which I managed to solved. I also administered Linux and the Webuzo panel. During working with Nuxt 3, creating another version of the site, I encountered many problems with Nuxt Markdown, which I managed to solve, despite gaps in the module's documentation."
       ],
     },
     {
@@ -24,10 +23,8 @@ export const info = {
       startDate: "Aug 2023",
       endDate: "Present",
       description: [
-        "- Working on features of Farmster, Farmportal and Foodpass.",
-        "- Fixing bugs with Farmster, Farmportal, Foodpass.",
-        "- DevOps managment.",
-        "- Managing SEO and quality of websites."
+        "My responsibilities: I worked on a number of farmer portals belonging to the FarmCloud platform, as well as their marketing pages. I mainly implemented frontend and application logic, as well as integrations with analytics systems. I corrected bugs and solved technical issues. Sometimes I designed individual UI elements. I managed SEO, occasionally managed DevOps and very rarely did anything on the backend. I regularly communicated with the team, and I also happened to present technical capabilities of application analytics to the client.",
+        "What I learned: I learned how to use and work in Agile mode with Azure Dev Ops. I mastered Angular version 14 through version 19. I also experienced working remotely in a larger team and working with Azure cloud services. I also learned an important lesson from the technical debt the team was facing."
       ],
     },
     {
@@ -36,9 +33,8 @@ export const info = {
       startDate: "Sep 2020",
       endDate: "Present",
       description: [
-        "- Managing school website.",
-        "- Developing new websites for school usecases.",
-        "- Fixing issues with school administration system.",
+        "My responsibilities: Creating the new school website was one of my larger projects. I planned the work with the team, starting with a survey that went out to all the school's students, to tailoring the various technical solutions to the problems we encountered. I was responsible for the UI/UX design along with the team's guidance, and implemented the SPA frontend in React with the team. I was the main designer of the data structure and configuration of Strapi headless CMS, coordinated the work of the entire team, which was done remotely in the form of weekly or daily meetings, managed tasks on the ClickUp platform using GitHub. In addition, I was responsible for hosting and maintaining the infrastructure of the new website of the Mari Sklodowska-Curie Vocational School Complex in Olesnica.",
+        "What I learned: Due to the versatility of the project, I gained skills in various IT fields. I learned a lot of things about SEO and taking care of the image of the site, I learned how to design UI/UX to a basic degree, I understood what mistakes to beware of when managing the work of others and how to transfer my knowledge in an effective way. It's also worth noting that this was the first time I dealt with DevOps production-wise. The whole project was a very valuable experience for me."
       ],
     },
 
@@ -48,9 +44,8 @@ export const info = {
       startDate: "April 2023",
       endDate: "Present",
       description: [
-        "- Managing klmat.pl website.",
-        "- Managing SEO and fixing issues.",
-        "- Managing Google Ads and brand promotion.",
+        "My responsibilities As an employee of KL MAT, I managed an online store based on ShopGold CMS. I added new products, took care of SEO and configured necessary modules on an ongoing basis. I also took care of the site's ads in Google Ads. In addition, I worked on maintaining the store on the hosting platform.",
+        "What did I learn: I learned how to manage an online store and configure modules, such as a payment gateway, as well as the impact of content quality on SEO. I also learned about the operation of the DNS server for the store's domain."
       ],
     },
   ],
